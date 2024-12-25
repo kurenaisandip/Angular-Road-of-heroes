d2436ccaa8aaefa8c415db385d3df617caea9fed
Sandip <srai68141@gmail.com>
2024-12-25 13:13:36 +0545

    5. Add navigation

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {HeroesComponent} from '../heroes/heroes.component';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'heroes', component: HeroesComponent},
+];
